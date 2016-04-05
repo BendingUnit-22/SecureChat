7064138bd28690415331192a151aa7bd829ff6a6
tickleberrie <wur1@wit.edu>
2016-04-05 14:33:16 -0400

    asd

--- SecureChat/out/production/upload.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cp -avr SecureChat/ ~/Documents/Shared/
-
--- SecureChat/src/ConnectCallback.java
+++
@@ -1,5 +0,0 @@
-
-public interface ConnectCallback {
-    public void ConnectionCallback(SecureConnection secCon);
-    public void didRecieveInfo(INFO info);
-}
